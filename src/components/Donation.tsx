b9b958d7d999cdf4d1de2cada0135b52682b9af7
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-12-16 15:11:51 +0100

    footer

--- src/components/Donation.tsx
+++ src/components/Donation.tsx
@@ -6,11 +6,11 @@ import { PAYPAL_URL, COFFEE_URL } from '@/../constants'
 import { useTranslations } from 'next-intl';
 
 const Donation = () => {
-   const t = useTranslations('subcontentJSON');
+   const t = useTranslations('uiJSON');
 
   return (
-    <div className="mt-8 text-center w-full">
-      <p>If you enjoyed this quiz, consider supporting us:</p>
+    <div className="mt-12 pt-6 text-center w-full">
+      <p className="mb-2 text-sm font-light">If you enjoyed this quiz, consider supporting us:</p>
       <div className="donation-container">
         <Link href={PAYPAL_URL} passHref target="_blank" rel="noopener noreferrer">
           <Button title="Donate via PayPal" btnIcon={<FaPaypal size={18} />} additionalStyles="donation-btn hover:bg-blue-500" />
@@ -19,8 +19,8 @@ const Donation = () => {
           <Button title="Buy Me a Coffee" btnIcon={<SiBuymeacoffee size={18} />} additionalStyles="donation-btn hover:bg-yellow-500" />
         </Link>
       </div>
-      <div className="container mx-auto text-center">
-        <p className="sm:text-sm">{t('footer')}</p>
+      <div className="container mx-auto text-center my-8">
+        <p className="sm:text-sm">{t('footerText')}</p>
         <p className="text-xs mt-1">
           &copy; {new Date().getFullYear()} Max Surnin. All rights reserved.
         </p>
