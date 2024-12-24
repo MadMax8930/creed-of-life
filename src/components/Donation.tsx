36af6ea03b7fb02124b2422d3b6d4951d8665086
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-12-24 14:53:48 +0100

    donation done

--- src/components/Donation.tsx
+++ src/components/Donation.tsx
@@ -9,19 +9,19 @@ const Donation = () => {
    const t = useTranslations('uiJSON');
 
   return (
-    <div className="mt-12 pt-6 text-center w-full">
-      <p className="mb-2 text-sm font-light">If you enjoyed this guide, consider supporting us:</p>
+    <div className="mt-12 pt-6 mb-4 text-center w-full">
+      <p className="donation-text">If you enjoyed this guide, consider supporting us:</p>
       <div className="donation-container">
         <Link href={PAYPAL_URL} passHref target="_blank" rel="noopener noreferrer">
-          <Button title="Donate via PayPal" btnIcon={<FaPaypal size={18} />} additionalStyles="donation-btn hover:bg-blue-500" />
+          <Button title="Donate via PayPal" btnIcon={<FaPaypal size={16} />} additionalStyles="donation-btn hover:bg-blue-500" />
         </Link>
         <Link href={COFFEE_URL} passHref target="_blank" rel="noopener noreferrer">
-          <Button title="Buy Me a Coffee" btnIcon={<SiBuymeacoffee size={18} />} additionalStyles="donation-btn hover:bg-yellow-500" />
+          <Button title="Buy Me a Coffee" btnIcon={<SiBuymeacoffee size={16} />} additionalStyles="donation-btn hover:bg-yellow-500" />
         </Link>
       </div>
-      <div className="container mx-auto text-center my-8">
-        <p className="sm:text-sm">{t('mainFooter')}</p>
-        <p className="text-xs mt-1">
+      <div className="footer-container">
+        <p className="footer-light">{t('mainFooter')}</p>
+        <p className="footer-bold">
           &copy; {new Date().getFullYear()} Max Surnin. All rights reserved.
         </p>
       </div>
