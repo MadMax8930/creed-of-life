355e176bf8d205a46c87771050166be72868585e
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2025-11-18 13:17:48 +0700

    statflows update v2

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,6 +15,13 @@ export default function Layout({children, title}: Props) {
       <Head>
         <title>{[title, t('descHead')].join(' - ')}</title>
         <link rel="icon" href="/favicon.ico" />
+        {/* Pixel Tracking Code */}
+        <script
+               dangerouslySetInnerHTML={{
+                  __html: `!function(t,a,n,e,o){try{var c=a.createElement("script");c.src="https://www.statflows.com/js/bundle.min.js",c.type="text/javascript",c.async=!0,c.onload=function(){t.statFlowAnalytics&&(t.sfa=t.statFlowAnalytics,t.statFlowAnalytics.init({appId:"0ca4f8602efc418fa84739543ed4290b",debugMode:!1,autoTrackPageEvents:!0,trackErrors:!0,trackPerformance:!0}))},c.onerror=function(){console.warn("Failed to load analytics from "+serverEndpoint)},a.head.appendChild(c)}catch(t){console.warn("Analytics initialization failed:",t)}}(window,document);`,
+               }}
+        />
+        {/* End Pixel Tracking Code */}
       </Head>
       <div className='min-h-screen flex flex-col items-center bg-primary'>
         {children}
